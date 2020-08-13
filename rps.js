7998e71913a7bcf1824ef59ce1b189fb46edf4d4
christine.scala22@gmail.com <christine.scala22@gmail.com>
2020-08-13 17:25:03 -0400

    trying to make it work

--- rps.js
+++ rps.js
@@ -1,47 +1,61 @@
 const computer=Math.floor(Math.random()*3);
-const rock=document.getElementById('rock').addEventListener("click", game())
-const paper=document.getElementById('paper').value
-const scissors=document.getElementById('scissors').value
 
 /*
-Rock > scissors
-Scissors > paper
-Paper > rock
-*/
+function userChoice (){
+    var rock=document.getElementById("rock").addEventListener("click", ());
+    var paper=document.getElementById("paper").addEventListener("click", ());
+    var scissors=document.getElementById("scissors').addEventListener("click", ());
+    if (==rock){
+    document.getElementById("choice").innerHTML = rock;
+    }
+    if (==scissors){
+    document.getElementById("choice").innerHTML = paper;
+    }
+    if (==paper){
+    document.getElementById("choice").innerHTML = scissors;
+    }
+}*/
+
 function computerChoice (){
-    if (computer == 1 return "rock";
-} else if {
-    computer == 2 return "paper";
-} else if {
-    computer == 3 return "scissors";
+    if (computer == 1) {
+        document.getElementById("random").innerHTML = rock;
+    } else if (computer == 2) {
+        document.getElementById("random").innerHTML = paper;
+    } else if (computer == 3) {
+        document.getElementById("random").innerHTML = scissors;
+    }
 }
+
+computerChoice ();
 
 function game (user, computer){
     if (rock,scissors) {
-        document.getElementById("result").innerHTML (You won!);
+        document.getElementById("result").innerHTML ("You won!");
     }
     else if (paper,scissors) {
-        document.getElementById("result").innerHTML (You Lost &#xe058;);
+        document.getElementById("result").innerHTML ("You Lost &#xe058;");
     }
     else if (scissors, scissors) {
-        document.getElementById("result").innerHTML (Draw);
+        document.getElementById("result").innerHTML ("Draw");
     }
     else if (paper,rock) {
-        document.getElementById("result").innerHTML (You won!);
+        document.getElementById("result").innerHTML ("You won!");
     }
     else if (scissors,rock) {
-        document.getElementById("result").innerHTML (You Lost &#xe058;);
+        document.getElementById("result").innerHTML ("You Lost &#xe058;");
     }
     else if (rock,rock) {
-        document.getElementById("result").innerHTML (Draw);
+        document.getElementById("result").innerHTML ("Draw");
     }
     else if (paper,paper) {
-        document.getElementById("result").innerHTML (Draw);
+        document.getElementById("result").innerHTML ("Draw");
     }
     else if (rock,paper) {
-        document.getElementById("result").innerHTML (You Lost &#xe058;);
+        document.getElementById("result").innerHTML ("You Lost &#xe058;");
     }
     else if (scissors, paper) {
-        document.getElementById("result").innerHTML (You won!);
+        document.getElementById("result").innerHTML ("You won!");
     }
 }
+
+game();
